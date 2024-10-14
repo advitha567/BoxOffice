346a32bb231b8d262af2b8fccccd4650a955d818
Advitha <advithabawgi@gmail.com>
2024-10-14 14:39:59 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,79 @@
+import React,{useState,useCallback} from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+import {apiGet} from '../misc/config';
+import { useLastQuery } from '../misc/custom-hooks';
+import { RadioInputsWrapper, SearchButtonWrapper, SearchInput } from './Home.styled';
+import CustomRadio from '../components/CustomRadio';
+
+const renderResults  = (results)=>{
+  if(results && results.length === 0){
+    return <div>No Results</div>;
+  }
+  if(results && results.length > 0){
+    return results[0].show? <ShowGrid data={results}/> : <ActorGrid  data={results}/>
+  }
+  return null;
+}
+
+
+const Home = () => {
+
+  const[input,setInput] = useLastQuery();
+  const[results,setResults] = useState(null);
+  const[searchOption,setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption==='shows';
+
+  const onInputChange = useCallback((ev) =>{
+    setInput(ev.target.value);
+  },[setInput])
+  const onSearch = ()=>{
+
+    apiGet(`/search/${searchOption}?q=${input}`).then(result =>{
+      setResults(result);
+    });
+  }
+  const onKeyDown = (ev)=>{
+    if(ev.keyCode === 13){
+      onSearch();
+    }
+  }
+
+  const onRadioChange = useCallback((ev)=>{
+    setSearchOption(ev.target.value);
+  },[]);
+
+  return (
+      <MainPageLayout>
+        <SearchInput 
+        type="text" 
+        onChange={onInputChange} 
+        onKeyDown={onKeyDown} 
+        value={input}
+        placeholder="Search for something"
+        />
+        
+        <RadioInputsWrapper>
+          <div>
+            <CustomRadio
+              label="Shows" id="shows-search" value="shows" checked={isShowsSearch} onChange={onRadioChange}
+            />
+          </div>
+          <div>
+            <CustomRadio 
+              label="Actors" id="actors-search"  value="people" checked={!isShowsSearch} onChange={onRadioChange}
+            />
+          </div>
+        </RadioInputsWrapper>
+        
+        <SearchButtonWrapper>
+          <button type="button" onClick={onSearch}>Search</button>
+        </SearchButtonWrapper>
+        {renderResults(results)}
+      </MainPageLayout>
+  )
+}
+
+export default Home
